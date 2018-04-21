b4ec359541db8a4e019d0f1be1065254816b1535
Christofer Herlin <christofer.herlin@gmail.com>
2018-04-21 22:12:58 +0200

    feat: implement persistence and rehydration of store

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { loadState, saveState } from './localStorage';
 
 import registerServiceWorker from './registerServiceWorker';
 import MapView from './containers/MapView';
 import './index.css';
 
-let store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ &&
+let store = createStore(reducers, loadState(), window.__REDUX_DEVTOOLS_EXTENSION__ &&
   window.__REDUX_DEVTOOLS_EXTENSION__());
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 ReactDOM.render(
   <Provider store={store}>
